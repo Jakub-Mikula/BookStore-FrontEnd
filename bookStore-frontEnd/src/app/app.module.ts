84f940f64a662fee04e42ba0dbc1bd27fc99083a
Jakub Mikula <jakub.mikula20@gmail.com>
2023-08-14 22:26:39 +0200

    feat(): Created other templates and folders needed for the project

--- bookStore-frontEnd/src/app/app.module.ts
+++ bookStore-frontEnd/src/app/app.module.ts
@@ -6,13 +6,21 @@ import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import { LoginComponent } from './guest/login/login.component';
 import { RegisterComponent } from './guest/register/register.component';
 import { HomeComponent } from './guest/home/home.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { AdminComponent } from './admin/admin/admin.component';
+import { NotFoundComponent } from './error/not-found/not-found.component';
+import { UnauthorizedComponent } from './error/unauthorized/unauthorized.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
+    ProfileComponent,
+    AdminComponent,
+    NotFoundComponent,
+    UnauthorizedComponent
   ],
   imports: [
     BrowserModule,
