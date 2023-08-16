2e818a2badeb601b4dddae2b794746c30a2c6688
Jakub Mikula <jakub.mikula20@gmail.com>
2023-08-16 12:35:01 +0200

    Added login and logout functions for the user

--- bookStore-frontEnd/src/app/guest/login/login.component.ts
+++ bookStore-frontEnd/src/app/guest/login/login.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { User } from "../../models/user.model";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import { AuthenticationService } from "../../service/authentication.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  user : User = new User();
+  faUser = faUserCircle;
+  errorMessage : string = "";
+
+  constructor(private authenticationService : AuthenticationService, private router : Router) {
+  }
+  ngOnInit(){
+    if (this.authenticationService.currentUserValue?.id){
+      this.router.navigate(['/profile']);
+      return
+    }
+  }
+
+  login(){
+    this.authenticationService.login(this.user).subscribe(data => {
+      this.router.navigate(['/profile'])
+    }, error => {
+      this.errorMessage = "Username or password is incorrect";
+      console.log("Username or password is incorrect")
+    });
+  }
 }
